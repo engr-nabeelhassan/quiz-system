db84346ee85b2ecce183d1b36c82e03736b81770
Nabeel Ahmed <nabeelkhoso46@gmail.com>
2025-05-01 22:32:50 +0500

    Validation nd Session

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,17 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 use App\Models\Admin;
 
 class AdminController extends Controller
 {
     //
-    function login(Request $req){
+    function login(Request $request){
+        
+        $validation = $request->validate([
+            "name"=>"required",
+            "password"=>"required"
+        ]);
 
         $admin = Admin::where([
-            ['name',"=",$req->name],
-            ['password',"=",$req->password]
+            ['name',"=",$request->name],
+            ['password',"=",$request->password]
         ])->first();
-        return view('admin',["name"=>$admin->name]);
+
+        if(!$admin){
+            $validation = $request->validate([
+                "user"=>"required",
+            ],[
+                "user.required"=>"User does not exist"
+            ]);
+        }
+        Session::put('admin',$admin);
+        return redirect('dashboard');
+    }
+
+    function dashboard(){
+        $admin = Session::get('admin');
+        if($admin){
+           return view('admin',["name"=>$admin->name]);
+        }else{
+            return redirect('admin-login');
+        }
     }
 }
